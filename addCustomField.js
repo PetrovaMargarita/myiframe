dbe492a13c24832c448c4bb9910222ec4d49ee18
PetrovaMargarita <34891678+PetrovaMargarita@users.noreply.github.com>
2024-06-20 21:26:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ addCustomField.js
@@ -0,0 +1,50 @@
+const pipedrive = require('pipedrive');
+require('dotenv').config();
+const defaultClient = new pipedrive.ApiClient();
+// Прямое указание API ключа
+const key = '2fa5095f3ba3911496e097a4cfde2f150e097539';
+defaultClient.authentications.api_key.apiKey = key;
+
+module.exports = (req, res) => {
+    // Добавление заголовков CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+res.setHeader('Access-Control-Allow-Credentials', true);
+
+
+    // Если это preflight запрос, просто завершите его
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
+    const { firstName, lastName, dealId } = req.body;
+
+    const api = new pipedrive.DealFieldsApi(defaultClient);
+    api.addDealField({
+        name: 'First Name',
+        field_type: 'varchar',
+    }).then(firstNameFieldResponse => {
+        const firstNameFieldKey = firstNameFieldResponse.data.key;
+
+        return api.addDealField({
+            name: 'Last Name',
+            field_type: 'varchar',
+        }).then(lastNameFieldResponse => {
+            const lastNameFieldKey = lastNameFieldResponse.data.key;
+
+            const dealsApi = new pipedrive.DealsApi(defaultClient);
+            const updateData = {
+                [firstNameFieldKey]: firstName,
+                [lastNameFieldKey]: lastName,
+            };
+
+            return dealsApi.updateDeal(dealId, updateData);
+        });
+    }).then(updatedDeal => {
+        res.status(200).json({ message: 'Deal updated successfully', data: updatedDeal });
+    }).catch(error => {
+        res.status(500).json({ message: 'Failed to add or update fields', error });
+    });
+};
